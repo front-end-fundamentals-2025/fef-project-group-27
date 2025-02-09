92a1132c04c491400cee5968226aadda72d81ba5
Edvinnn22 <edvin.hultqvist@icloud.com>
2025-02-09 23:15:40 +0100

    productspage basic layout

---
+++ JS/filter.js
@@ -0,0 +1,9 @@
+function toggleFilter() {
+    const filterPopup = document.getElementById('filterPopup');
+    if (filterPopup.style.display === 'block') {
+        filterPopup.style.display = 'none';
+    } else {
+        filterPopup.style.display = 'block';
+    }
+}
+
