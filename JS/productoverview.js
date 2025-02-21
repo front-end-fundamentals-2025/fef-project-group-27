e3f66bdec751d62b61e88b5041c3c989134eca86
Edvinnn22 <edvin.hultqvist@icloud.com>
2025-02-21 16:03:13 +0100

    image hover

---
+++ JS/productoverview.js
@@ -0,0 +1,8 @@
+const smallImages = document.querySelectorAll(".small-image");
+const mainImage = document.getElementById("main-image");
+
+smallImages.forEach((image) => {
+  image.addEventListener("mouseover", () => {
+    mainImage.src = image.src; // Update the main image source
+  });
+});
---
+++ JS/productpage.js
@@ -0,0 +1,7 @@
+ 
+ const image = document.getElementById("clickableImage");
+
+
+ image.addEventListener("click", function() {
+     window.location.href = "productOverview.html";
+ });
